2c4bcc90d266d39d73b73aa980e29a188bd7b6f4
CharlyWelch <blwbiology@gmail.com>
2017-12-11 16:25:25 -0800

    creat update form and routes

--- scripts/routes.js
+++ scripts/routes.js
@@ -1,11 +1,12 @@
-page('/', (ctx, next) => {
-    app.Book.fetchAll(app.bookView.initHome);
-});
+page('/', app.Book.fetchAll, app.bookView.initHome);
 
 page('/books/new', app.bookView.initNewBook);
 
+page('/books', app.Book.fetchAll, app.bookView.initHome);
 page('/books/:id', app.Book.fetchOne, app.bookView.initDetailPage);
 page('/books/:id/update', app.book.fetchOne, app.bookView.initUpdatePage);
+
+// page('/about', app.aboutView.initAboutPage);
 
 page.base('/books-client');
 page.start();
