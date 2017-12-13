35b63f5a178cd7de5df0711e011d3b59141da5b2
Carmen Perezchica <crperezchica@gmail.com>
2017-12-12 18:23:45 -0800

    moved page('//search up

--- scripts/routes.js
+++ scripts/routes.js
@@ -2,11 +2,11 @@ page('/', app.Book.fetchAll, app.bookView.initHome);
 
 
 page('/books', app.Book.fetchAll, app.bookView.initHome);
+page('/books/search', app.bookView.initSearchFormPage);
 page('/books/new', app.bookView.initNewBook);
 page('/books/:id', app.Book.fetchOne, app.bookView.initDetailPage);
 page('/books/:id/update', app.Book.fetchOne, app.bookView.initUpdatePage);
 
-page('/books/search', app.bookView.initSearchFormPage);
 // page('/about', app.aboutView.initAboutPage);
 
 page.base('/books-client');
